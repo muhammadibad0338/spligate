a31f87703b9a4f9e5a8e89794b2912221321e227
Muhammad Ibad <mibad0338@gmail.com>
2022-09-01 20:19:20 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import Layout from './Layout/Layout';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Nunito-Regular", "sans-serif"].join(","),
+    fontSize: 12,
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        height: 40,
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme} >
+      <Layout />
+    </ThemeProvider>
   );
 }
 
---
+++ src/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Home from '../Screens/Home'
+
+const Layout = () => {
+  return (
+    <Home/>
+  )
+}
+
+export default Layout
