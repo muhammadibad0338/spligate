0149f1676947a3cbca5939de9eefae881cafc276
Muhammad Ibad <mibad0338@gmail.com>
2022-09-12 02:15:36 +0500

    about updating

--- src/Screens/About/Components/SectionOne.js
+++ src/Screens/About/Components/SectionOne.js
@@ -7,6 +7,17 @@ import {
 import { makeStyles } from "@mui/styles";
 import bg1 from "../../../Assets/Images/aboutBanner.png"
 import darkBg from "../../../Assets/Images/homeDarkbg.png";
+import State from "../../../State/About.json"
+import MainHeading from '../../../Components/MainHeading';
+import studioLogo1 from "../../../Assets/Images/studioLogo1.png";
+import studioLogo2 from "../../../Assets/Images/studioLogo2.png";
+import studioLogo3 from "../../../Assets/Images/studioLogo3.png";
+import studioLogo4 from "../../../Assets/Images/studioLogo4.png";
+import studioLogo5 from "../../../Assets/Images/studioLogo5.png";
+import studioLogo6 from "../../../Assets/Images/studioLogo6.png";
+import studioLogo7 from "../../../Assets/Images/studioLogo7.png";
+
+
 
 const useStyles = makeStyles((theme) => ({
     banner1: {
@@ -25,93 +36,68 @@ const useStyles = makeStyles((theme) => ({
             backgroundSize: 'auto, contain,cover',
             backgroundPosition: '0px 0px, 50% 0%,50% 0%',
             backgroundRepeat: 'repeat,no-repeat,repeat-y',
-            height:'100vh'
+            height: '100vh'
         },
     },
-    banner1MainCntnr: {
-        display: 'flex',
-        flexDirection: 'column !important',
-        alignItems: 'flex-start',
-        height: '100vh',
-        maxHeight: '2160px',
-        minHeight: '700px',
-        paddingTop: '10rem',
-        [theme.breakpoints.down("fl")]: {
-            height: '100vh',
-            minHeight: '900px',
-            paddingTop: '80px',
-        },
-        [theme.breakpoints.down("md")]: {
-            minHeight: '800px',
-            // justifyContent: "center",
-            alignItems: 'center',
-            paddingTop: '60px'
-        },
-        [theme.breakpoints.down("sm")]: {
-            paddingTop: '10rem'
-        },
-        [theme.breakpoints.down("m")]: {
-            paddingTop: '50%'
-        }
-    },
-    llogo: {
-        display: 'inline-block',
-        flex: '0 auto',
-        maxHeight: 'none',
-        minHeight: '0px',
-        maxWidth: '400px',
-        paddingLeft: '35px',
-        marginBottom: '40px',
-        [theme.breakpoints.down("md")]: {
-            display: 'block',
-            width: '90%',
-            height: 'auto',
-            maxWidth: '350px',
-            marginBottom: '40px',
-        },
-        [theme.breakpoints.down("sm")]: {
-            display: 'block',
-            width: '80%',
-            height: 'auto',
-            maxWidth: '300px',
-            marginBottom: '40px',
-        }
-    },
-    smallerTitle: {
-        // '& .MuiTypography-root': {
-        fontSize: '1.3rem !important',
+    desc1: {
+        width: '60vw',
+        height:'auto',
+        minWidth:'900px',
+        fontSize: '1.5rem !important',
         lineHeight: '30px',
         fontFamily: ["Motserrat", "sans-serif"],
         color: 'white',
         fontWeight: '500 !important',
-        textAlign: 'left',
-        marginBottom: '5px !important',
-        marginLeft: '35px !important',
+        textAlign: 'center',
+        marginTop: '10px !important',
+        textShadow: '0 3px 12px rgb(0 0 0 / 50%) !important',
         [theme.breakpoints.down("md")]: {
-            fontSize: '1.2rem !important',
+            fontSize: '1.3rem !important',
             lineHeight: '30px',
             textAlign: 'center',
             marginBottom: '5px !important',
-            marginLeft: '0px !important'
+            width: '70vw',
+            minWidth:'580px',
         },
         [theme.breakpoints.down("sm")]: {
-            fontSize: '1.1rem !important',
+            fontSize: '1.3rem !important',
             lineHeight: '30px',
             textAlign: 'center',
+            width: '90vw',
+            minWidth:'250px',
             marginBottom: '10px !important',
-            marginLeft: '0px !important'
         }
-        // }
     },
+    gap: {
+        display: 'block',
+        minHeight: '65px',
+        [theme.breakpoints.down("md")]: {
+            height: '5vh'
+        },
+        [theme.breakpoints.down("sm")]: {
+            height: '75px'
+        }
+    }
 }));
 
 const SectionOne = () => {
     const classes = useStyles();
+    const logoArray = [studioLogo1,studioLogo2,studioLogo3,studioLogo4,studioLogo5,studioLogo6,studioLogo7]
     return (
         <Grid container className={classes.banner1} >
-            <Grid item xs={12} >
-                
+            <Grid item xs={12} sx={{ display: 'flex', flexDirection: 'column',alignItems:'center' }} >
+                <div className={classes.gap} ></div>
+                <MainHeading text={State.head1}
+                    sx={{
+                        color: 'white',
+                        textAlign: 'center',
+                        textShadow: '0 3px 12px rgb(0 0 0 / 50%)',
+                        fontWeight: '800'
+                    }}
+                />
+                <Typography className={classes.desc1} >{State.desc1}</Typography>
             </Grid>
+
         </Grid>
     )
 }
