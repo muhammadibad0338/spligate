2f22bcc998108a471efeeb681414094be37f9880
Muhammad Ibad <mibad0338@gmail.com>
2022-09-12 18:22:33 +0500

    about page completed

--- src/Screens/About/About.js
+++ src/Screens/About/About.js
@@ -1,13 +1,4 @@
 import React from 'react'
-import {
-    Typography,
-    Grid,
-    Box,
-    Container
-} from "@mui/material";
-import { makeStyles } from "@mui/styles";
-import { styled } from '@mui/system';
-import darkBg from "../../Assets/Images/homeDarkbg.png"
 import SectionOne from './Components/SectionOne';
 import SectionTwo from './Components/SectionTwo';
 
