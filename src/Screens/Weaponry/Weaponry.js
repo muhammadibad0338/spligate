6851b4f5ce91209d73d340117cdaea45e99d4906
Muhammad Ibad <mibad0338@gmail.com>
2022-09-13 21:43:03 +0500

    start working on weaponry

--- src/Screens/Weaponry/Weaponry.js
+++ src/Screens/Weaponry/Weaponry.js
@@ -6,9 +6,42 @@ import {
 import { makeStyles } from "@mui/styles";
 import darkBg from "../../Assets/Images/homeDarkbg.png";
 
+
+const useStyles = makeStyles((theme) => ({
+    banner: {
+        // height: 'auto',
+        height:'100vh',
+        width: '100%',
+        backgroundImage: `url(${darkBg})`,
+        backgroundSize: 'auto, cover,cover',
+        backgroundPosition: '0px 0px, 50% 0%,50% 0%',
+        backgroundRepeat: 'repeat,no-repeat,repeat-y',
+        [theme.breakpoints.down("fl")]: {
+            backgroundSize: 'auto,cover,cover',
+        },
+        [theme.breakpoints.down("md")]: {
+            backgroundImage: `-webkit-linear-gradient(180deg, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15)),url(${darkBg})`,
+            backgroundSize: 'auto, cover,cover',
+            backgroundPosition: '0px 0px, 82% 0%,50% 0%',
+            backgroundRepeat: 'repeat,no-repeat,repeat-y'
+        },
+    },
+}));
+
 const Weaponry = () => {
+
+    const classes = useStyles();
+
     return (
-        <div>Weaponry</div>
+        <div className={classes.banner}  >
+            {/* <MainHeading text={State.mainHead} sx={{
+                color: 'white',
+                textAlign: 'center',
+                textShadow: '0 3px 12px rgb(0 0 0 / 50%)',
+                fontWeight: '800',
+                padding: { lg: '60px 0px', sm: '30px 0px', xs: '10px 0px' }
+            }} /> */}
+        </div>
     )
 }
 
