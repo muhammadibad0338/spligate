9321080dc2fc6d9bd5ed46bc3d4651942a25ae52
Muhammad Ibad <mibad0338@gmail.com>
2022-09-14 09:05:14 +0500

    updating

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -2,21 +2,15 @@ import React from 'react';
 import Routes from '../Routes/Routes';
 import { useTheme, useMediaQuery } from "@mui/material/";
 import NavBar from '../Components/NavBar';
-import MobileNav from '../Components/MobileNav';
 
 
 const Layout = () => {
     const theme = useTheme();
 
-    const largeScreens = useMediaQuery(theme.breakpoints.up("md"));
 
     return (
         <React.Fragment>
-            {
-                true ?
-                    <NavBar/> :
-                    <MobileNav/>
-            }
+            <NavBar />
             <Routes />
         </React.Fragment>
     )
